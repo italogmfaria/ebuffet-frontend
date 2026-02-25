84fd71569e168714a0e55e75508b305c37a5196b
João Gabriel <118997962+joaog0liveira@users.noreply.github.com>
2026-02-25 19:14:35 -0300

    hotfix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   buffetId: 1,
-  API_URL: 'https://interfulgent-coefficiently-elinore.ngrok-free.dev/api',
+  API_URL: 'http://localhost:8080/api',
   TENANT_HEADER: 'X-Buffet-Id'
 };
